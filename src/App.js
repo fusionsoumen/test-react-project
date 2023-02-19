62f7407c860362f3140d2f2629ca52294db45858
Soumen Sardar <soumen.wad@gmail.com>
2023-02-19 22:12:09 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
 import BannerComponent from './components/BannerComponent';
+import AboutComponent from './components/AboutComponent';
+import ServicesComponent from './components/ServicesComponent';
+import ProjectsComponent from './components/ProjectsComponent';
+import BlogComponent from './components/BlogComponent';
+import ContactComponent from './components/ContactComponent';
 import FooterComponent from './components/FooterComponent';
+
+
 function App() {
   return (
     <div className="App">
       <HeaderComponent></HeaderComponent>
       <BannerComponent></BannerComponent>
+      <AboutComponent></AboutComponent>
+      <ServicesComponent></ServicesComponent>
+      <ProjectsComponent></ProjectsComponent>
+      <BlogComponent></BlogComponent>
+      <ContactComponent></ContactComponent>
       <FooterComponent></FooterComponent>
+     
     </div>
   );
 }
